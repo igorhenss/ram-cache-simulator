10f7ffd8f66603dd47628cb3c6dae046dbb1b538
igorhenss <igorhenss@gmail.com>
2020-01-17 15:25:46 -0300

    Project created.

---
+++ src/main/java/com/igorhenss/ramcache/ramcachesimulator/RamCacheSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.igorhenss.ramcache.ramcachesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamCacheSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamCacheSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorhenss/ramcache/ramcachesimulator/RamCacheSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorhenss.ramcache.ramcachesimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamCacheSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
